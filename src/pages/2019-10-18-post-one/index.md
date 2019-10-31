447de0a73df8fe75bbc788647f61918fc7e81074
geraldbrianp <57222273+geraldbrianp@users.noreply.github.com>
2019-10-31 20:35:27 +0700

    Add files via upload

---
+++ src/pages/2019-10-18-post-one/index.md
@@ -0,0 +1,8 @@
+---
+path: "/post-one"
+date: "2019-10-18"
+title: "My First Blog Post"
+author: "John Doe"
+---
+
+Hi there, welcome to my GatsbyJS blog. This is my first post.
---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+    
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+    
+const AboutPage = () => (
+  <Layout>
+    <SEO title="About Us" />
+    <h1>About Us</h1>
+    <p>We are Web Developers.</p>
+  </Layout>
+)
+    
+export default AboutPage
